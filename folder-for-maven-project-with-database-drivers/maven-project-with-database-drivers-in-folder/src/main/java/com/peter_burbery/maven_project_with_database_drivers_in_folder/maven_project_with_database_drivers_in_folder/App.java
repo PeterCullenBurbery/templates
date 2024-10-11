d9c49d7c7fbb0deb0200d20c37f53566367c8ffa
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-10-11 05:20:56 -0400

    I created a Maven template.
    I tried to create one based on off ChatGPT approach, but that didn't
    work, so I created a Maven project, added Oracle OJDBC driver 11 to
    pom.xml and updated the dependencies when building, then copied this
    folder/project over to this repository so it can be used as a template.

---
+++ folder-for-maven-project-with-database-drivers/maven-project-with-database-drivers-in-folder/src/main/java/com/peter_burbery/maven_project_with_database_drivers_in_folder/maven_project_with_database_drivers_in_folder/App.java
@@ -0,0 +1,10 @@
+package com.peter_burbery.maven_project_with_database_drivers_in_folder.maven_project_with_database_drivers_in_folder;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
